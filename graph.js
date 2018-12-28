a07ba480243459cdfff961e6ce1fb68baab2f379
Lance Owen Merrill <Lanceom@hawaii.edu>
2018-12-28 12:38:41 -0800

    finsihed project

--- graph.js
+++ graph.js
@@ -19,35 +19,73 @@ const arcPath = d3.arc()
 
 const color = d3.scaleOrdinal(d3['schemeDark2'])
 
+const legendGroup = svg.append('g')
+    .attr('transform', `translate(${dims.width + 40}, 10)`);
+const legend = d3.legendColor()
+    .shape('circle')
+    .shapePadding(10)
+    .scale(color);
+
+const tip = d3.tip()
+    .attr('class', 'tip card')
+    .html(d => {
+        let content = `<div class="name">Name: ${d.data.name}</div>`;
+        content +=`<div class="cost">Cost: ${d.data.cost}</div>`;
+        content +=`<div class="delete">Click Slice to delete</div>`;
+        return content;
+    })
+
+    graph.call(tip);
+
 // update function 
 const update = (data) => {
 
+
+
     color.domain(data.map(d => d.name));
+    legendGroup.call(legend);
+    legendGroup.selectAll('text')
+        .attr('fill', 'white')
 
 
     // join enchanced(pie) data to path elements
     const paths = graph.selectAll('path')
         .data(pie(data));
 
-        //removes uneeded documents and paths from DOM to update
-        
-        paths.exit().remove();
+        //removes unneeded documents and paths from DOM to update
 
+        paths.exit()
+            .transition().duration(1000)
+            .attrTween('d', arcTweenExit)
+            .remove();
+         
         paths
-            .attr('class', 'acr')
             .attr('d', arcPath)
-            .attr('stroke', '#fff')
-            .attr('stroke-width', '1')
-            .attr('fill', d => color(d.data.name));
+            .transition().duration(1000)
+            .attrTween('d', arcTweenUpdate)
+
     
         paths.enter()
             .append('path')
                 .attr('class', 'acr')
-                .attr('d', arcPath)
                 .attr('stroke', '#fff')
                 .attr('stroke-width', '1')
                 .attr('fill', d => color(d.data.name))
+                .each(function(d) { this._current = d })
+                .transition().duration(1000)
+                    .attrTween('d', arcTweenEnter);
 
+        // add event listerns
+        graph.selectAll('path')
+        .on('mouseover', (d, i , n) => {
+            tip.show(d, n[i])
+            handleMouseOver(d, i , n)
+        })
+        .on('mouseout', (d, i , n) => {
+            tip.hide();
+            handleMouseOut(d, i , n);
+        })
+        .on('click', handleClick)
 }
 
 //data array and firestore
@@ -69,11 +107,58 @@ db.collection('expenses').onSnapshot(res => {
                 data[index] = doc;
                 break;
             case 'removed':
-                data = data.filter(item = item.id !== doc.id);
+                data = data.filter(item => item.id !== doc.id);
                 break;
             default:
                 break;
         }
     });
     update(data)
-})
+});
+
+const arcTweenEnter = (d) => {
+    var i = d3.interpolate(d.endAngle, d.startAngle)
+
+    return function(t){
+        d.startAngle = i(t);
+        return arcPath(d)
+    };
+};
+
+const arcTweenExit = (d) => {
+    var i = d3.interpolate(d.startAngle, d.endAngle)
+
+    return function(t){
+        d.startAngle = i(t);
+        return arcPath(d)
+    };
+};
+
+// use function keyword to allow .this
+
+function arcTweenUpdate(d) {
+    // interpolate between two objects 
+    var i = d3.interpolate(this._current, d);
+    //update current prop with new data 
+    this._current = i(1);
+
+    return function(t) {
+        return arcPath(i(t))
+    }
+}
+
+const handleMouseOver = (d, i, n) => {
+    d3.select(n[i])
+        .transition('changeSliceFill').duration(200)
+            .attr('fill', '#fff')
+}
+
+const handleMouseOut = (d, i, n) => {
+    d3.select(n[i])
+        .transition('changeSliceFill').duration(200)
+            .attr('fill', color(d.data.name))
+}
+const handleClick = (d) => {
+    const id = d.data.id;
+    db.collection('expenses').doc(id).delete();
+}
